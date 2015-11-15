760bdd292dcf173b82255dfb5dc6405955cab450
jessiewillms <willms.jessie@gmail.com>
2015-11-15 10:32:46 -0500

    initial commit

--- scripts/js.js
+++ scripts/js.js
@@ -1,23 +1,26 @@
 var app = {};
 
+
+function getJson(json) {
+
+    var content = "";
+
+    h1 = json.feed.entry[0].gsx$h1.$t;
+
+    for (var i = 0; i < json.feed.entry.length; i++) {
+        entry = json.feed.entry[i];
+
+        var copy = entry.gsx$copy.$t;
+
+        content += '<div class="wrap">' +  + '</div>';
+    }
+    $('.wrapper').append(content);
+};
+
+
 app.init = function() {
 
-	function getJsonFromGoogle(json) {
 
-	    var content = "";
-	    var currentdate;
-
-	    h1 = json.feed.entry[0].gsx$h1.$t;
-
-	    for (var i = 0; i < json.feed.entry.length; i++) {
-	        entry = json.feed.entry[i];
-
-	        var copy = entry.gsx$copy.$t;
-
-	        content += '<div class="wrap">' +  + '</div>';
-	    }
-	    $('.wrapper').append(content);
-	};
 };
 
 $(function() {
