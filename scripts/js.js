571f2a90704d2cf441fdc4843abe1938b22d0383
jessiewillms <willms.jessie@gmail.com>
2015-11-15 13:14:19 -0500

    adds next button

--- scripts/js.js
+++ scripts/js.js
@@ -1,17 +1,18 @@
 var app = {
 	current_room: "",
 	selected_activity: "",
-	active_array: []
+	active_array: [],
+	counter: 0
 };
 
 var data = {
 	"step1" : {
-		"joke": "lorem joke one",
+		"joke": "step1 joke 1 lorem joke one",
 		"joke1": "lorem joke two",
 		"joke2": "lorem joke three"
 	},
 	"step2" : {
-		"joke": "lorem joke one",
+		"joke": "step2 joke 1 lorem joke one",
 		"joke1": "lorem joke two",
 		"joke2": "lorem joke three"
 	}
@@ -21,6 +22,14 @@ var data = {
 console.log(data.step1.joke)
 
 
+$('.next').click(function(){
+	app.counter++;
+	var steps = ".step" + app.counter;
+	$(steps).show();
+
+
+	$(steps).append('<li>' + data.step1.joke + '</li>')
+});
 
 app.eureka = function() {
 	var text = $('h1').html().split('');
