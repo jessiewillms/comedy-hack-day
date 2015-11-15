87b300e2124e3fca92fe880f181c61fc0acd3ad1
jessiewillms <willms.jessie@gmail.com>
2015-11-15 11:30:32 -0500

    adds buttons

--- scripts/js.js
+++ scripts/js.js
@@ -1,16 +1,25 @@
-var app = {};
+var app = {
+	current_room: "",
+	selected_activity: ""
+};
 
+$('.step1 ul li').click(function(){
+	app.current_room = $(this).attr('data-room');
 
+});
 
-$('li').click(function(){
-	var current_room = $(this).attr('data-room');
+$('.step2 ul li').click(function(){
+	app.selected_activity = $(this).attr('data-activity');
+});
 
-	
+$('.step3 button.submit').click(function(){
+	console.log(app.selected_activity);
+	console.log(app.current_room)
 });
 
 app.eureka = function() {
 	var text = $('h1').html().split('');
-	console.log(text)
+	// console.log(text)
 	// $('h1').fadeIn('slow', function () {
 	//   $(this).delay(5000).fadeOut('slow');
 	// });
