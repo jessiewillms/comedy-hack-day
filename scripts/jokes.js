255942f91081abad76090c7cc07d43443136cd4b
jessiewillms <willms.jessie@gmail.com>
2015-11-15 20:25:21 -0500

    adds finl vesion to go live

--- scripts/jokes.js
+++ scripts/jokes.js
@@ -9,9 +9,52 @@ $('.no').click(function(){
 	$(this).parent().parent().hide();
 	$(this).parent().parent().next().show();
 
-	$('img:not(.mb-img)').delay('600').fadeIn();
-	$('h2 span').delay('600').fadeIn();
+	$('img').delay('1200').fadeIn();
+	$('h2 span').delay('1200').fadeIn();
 });
+
+
+
+
+$('#step1').click(function(){
+
+	$('.first-screen').hide();
+	$('.something-else').hide();
+	$('.option').hide();
+	$('.wrap').hide();
+
+	$('h2 span').delay('1200').fadeIn();
+	$('.img-wrap').delay('1300').fadeIn();
+
+	$('.step1').show();
+	$('#option-1').show();
+
+
+
+	
+});
+
+
+$('#step2').click(function(){
+	console.log('sup')
+	$('#donotshow').hide();
+
+	$('h2 span').hide();
+	$('.img-wrap').hide();
+
+	$('.first-screen').hide();
+	$('.something-else').hide();
+	$('.option').hide();
+	$('.wrap').hide();
+
+
+	$('h2 span').delay('1800').fadeIn();
+	$('.img-wrap').delay('1800').fadeIn();
+
+	$('.step2').show();
+	$('#option-2').show();
+});
+
 
 $('#step3').click(function(){
 	console.log('three')
@@ -23,33 +66,8 @@ $('#step3').click(function(){
 	$('.step3').show();
 	$('#option-3').show();
 
-	$('h2 span').delay('600').fadeIn();
-});
-
-$('#step2').click(function(){
-	console.log('teo')
-	$('.first-screen').hide();
-	$('.something-else').hide();
-	$('.option').hide();
-	$('.wrap').hide();
-
-	$('.step2').show();
-	$('#option-2').show();
-
-	$('h2 span').delay('600').fadeIn();
-});
-
-$('#step1').click(function(){
-
-	$('.first-screen').hide();
-	$('.something-else').hide();
-	$('.option').hide();
-	$('.wrap').hide();
-
-	$('.step1').show();
-	$('#option-1').show();
-
-	$('h2 span').delay('600').fadeIn();
+	$('h2 span').delay('1200').fadeIn();
+	$('.img-wrap').delay('1300').fadeIn();
 });
 
 
@@ -65,7 +83,7 @@ $('.three-bttn-wrap button#step1').click(function(){
 	$('.step1').show();
 	$('#option-1').show();
 
-	// $('h2 span').delay('600').fadeIn();
+	// $('h2 span').delay('1200').fadeIn();
 
 });
 
@@ -75,7 +93,7 @@ $('.three-bttn-wrap button#step2').click(function(){
 	$('.step2').show();
 	$('#option-2').show();
 
-	// $('h2 span').delay('600').fadeIn();
+	// $('h2 span').delay('1200').fadeIn();
 });
 
 $('.three-bttn-wrap button#step3').click(function(){
@@ -84,16 +102,39 @@ $('.three-bttn-wrap button#step3').click(function(){
 	$('.step3').show();
 	$('#option-3').show();
 
-	// $('h2 span').delay('600').fadeIn();
+	// $('h2 span').delay('1200').fadeIn();
 });
 
 $('.last').click(function() {
-	$('.option').hide();
-	$('.wrap').hide();
-	$('.mb-wrapper').show();
-	$('.mb').show()
+    $('.option').hide();
+    $('.wrap').hide();
+    $('.mb-wrapper').show();
+    $('.mb').show();
 });
 
-$('.mb').click(function(){
-	$('.mb-img').show();
+$('.mb').click(function() {
+
+    $(".loading").show();
+    setTimeout(function() {
+        $(".loading").remove();
+    }, 3000);
+
+    $('.mb').hide();
+    $('.mb-wrapper h2').hide();
+    $('#masterbate').show();
+
+    // .delay('300').remove();
+
+    // $(this).hide();
+    // $('.mb-wrapper h2').hide();
+    // $('.mb-img').show();
 })
+
+$('.yes').click(function(){
+	$('.pop-up').show();
+});
+
+
+$(window).scroll(function(){
+	$('.placeholder').remove();
+});
